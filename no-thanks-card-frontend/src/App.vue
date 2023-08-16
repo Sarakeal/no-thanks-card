01b4582ee1064187dd95a4dc0156bf663a62e31d
shen <739343447@qq.com>
2023-08-16 22:03:16 +0800

    Initial frontend with Vue2

---
+++ no-thanks-card-frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <HomePage msg="Home"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HomePage from './pages/HomePage.vue';
+
+@Component({
+  components: {
+    HomePage: HomePage,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+
+</style>
---
+++ no-thanks-card-frontend/src/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-page">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
