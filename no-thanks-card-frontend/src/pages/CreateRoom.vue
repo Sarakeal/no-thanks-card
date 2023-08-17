b8685fa1a47017bc59ab5aa70dde9e34b6d11eb1
shen <739343447@qq.com>
2023-08-17 23:00:38 +0800

    Add create room input

--- no-thanks-card-frontend/src/pages/CreateRoom.vue
+++ no-thanks-card-frontend/src/pages/CreateRoom.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="main-page">
-    <button>确认</button>
+  <div class="createroom">
+    <div class="name">
+      <span class="hint">你的昵称：</span>
+        <input
+            :maxlength="10"
+            type="text"
+            placeholder="请输入昵称"
+            v-model="nickname"
+        />
+    </div>
+    <div class="password">
+      <span class="hint">房间密码：</span>
+        <input
+            type="text"
+            :maxlength="10"
+            placeholder="(可选)"
+            v-model="password"
+        />
+    </div>
+    <button>确认创建</button>
   </div>
 </template>
 
@@ -9,8 +27,11 @@ import {setup} from "@/socket";
 
 export default {
   name: 'CreateRoom',
-  props: {
-    msg: String
+  data() {
+    return {
+      nickname: '',
+      password: ''
+    }
   },
   created() {
     setup();
@@ -18,6 +39,50 @@ export default {
 }
 </script>
 
-<style scoped>
 
+<style lang="scss" scoped>
+.createroom {
+  padding: 1rem 1rem 0;
+  text-align: center;
+  .title {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+  .tile-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    .room-character-tile {
+      flex: 1 1 33%;
+      padding: 1rem 0;
+    }
+  }
+
+  .name,
+  .password {
+    .hint {
+      position: relative;
+      bottom: 0.04em;
+      word-break: keep-all;
+      margin-right: 0.5rem;
+      font-weight: bold;
+    }
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 2rem 0;
+    input {
+      max-width: calc(100% - 1rem);
+      padding: 0 0.5rem;
+      line-height: 2.4rem;
+      overflow: visible;
+    }
+    .useborder {
+      max-width: 50%;
+    }
+  }
+
+  .btn {
+    margin: auto;
+  }
+}
 </style>
