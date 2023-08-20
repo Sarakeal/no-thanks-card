b3caaf9a8be2a5cf6ccb8ec4a982152c35f1bdfb
shen <739343447@qq.com>
2023-08-20 20:40:28 +0800

    Get winner

--- no-thanks-card-backend/src/handlers/http/game/index.ts
+++ no-thanks-card-backend/src/handlers/http/game/index.ts
@@ -98,7 +98,16 @@ export const GameHandler: IGameHandler = {
   },
 
   end(room: Room) {
-    const winner = room.getPlayers()[0]; // FIXME
+    let winner = room.getPlayers()[0];
+    let minScore = 9999;
+    room.getPlayers().forEach(player => {
+      let score = calcScore(player.cards);
+      // TODO 可能有多个最小分数者
+      if (score < minScore) {
+        minScore = score;
+        winner = player;
+      }
+    })
     io.to(room.roomNumber).emit(Events.GAME_END, {
       winner,
     } as GameEndMsg);
@@ -114,6 +123,31 @@ export const GameHandler: IGameHandler = {
       Room.clearRoom(room.roomNumber);
     }, CLEAR_ROOM_TIME);
   }
+}
+
+
+function calcScore(pCards) {
+  let res = []; // 双层数组
+  // 如果数字能连在一起，只统计上最小的数字作为分数，比如：3，4，5，7，最终分数为3+7=10
+  let cards = [...pCards];
+  cards.sort();
+  for (let i = 0; i < cards.length; i++) {
+    let j = i;
+    while (j + 1 < cards.length && cards[j + 1] === cards[j] + 1) {
+      j++;
+    }
+    let arr = [];
+    for (let k = i; k <= j; k++) {
+      arr.push(cards[k]);
+    }
+    res.push(arr);
+    j = i;
+  }
+  let score = 0;
+  res.forEach(arr => {
+    score += arr[0];
+  });
+  return score;
 }
 
 export class Cards {
