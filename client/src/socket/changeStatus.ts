1c8fb0c2b9727ab5295ce25e9f0c6ca1c856b362
shen <739343447@qq.com>
2023-08-28 22:48:13 +0800

    set time out

--- client/src/socket/changeStatus.ts
+++ client/src/socket/changeStatus.ts
@@ -1,7 +1,10 @@
 import {ChangeStatusMsg} from "../../shared/wsMsg/ChangeStatus";
 import store from "@/store";
+import {AnimationConfig} from "../../shared/constants";
 
 export default function changeStatus(msg: ChangeStatusMsg) {
-  store.commit('setPlayers', msg.players);
   store.commit('setGameInfo', msg.gameInfo);
+  setTimeout(() => {
+    store.commit('setPlayers', msg.players);
+  }, AnimationConfig.animationTime);
 }
--- client/src/socket/gameEnd.ts
+++ client/src/socket/gameEnd.ts
@@ -7,5 +7,7 @@ export default function gameEnd(msg: GameEndMsg) {
   socket.disconnect();
 
   console.log('Game end');
-  showDialog(`<b>游戏结束</> </br>获胜者是 ${msg.winner.name}`);
+  setTimeout(() => {
+    showDialog(`<b>游戏结束</> </br>获胜者是 ${msg.winner.name}`);
+  }, 2000);
 }
